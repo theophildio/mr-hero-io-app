a046ea2097d083f4453c74b74210be625cb47b90
Theophil Dio <theophildio@Theophil-Dio-M2.local>
2026-03-18 22:53:49 +0600

    Fixed design to Navbar and added Banner to repo

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router";
+import Git from "../../assets/github.png";
+import Logo from "../../assets/logo.png";
+
 const Navbar = () => {
+
+    const links = <>
+        <Link to="/" className="mr-4">Home</Link>
+        <Link to="/app" className="mr-4">Apps</Link>
+        <Link to="/installation" className="mr-0">Installation</Link>
+    </>
+
   return (
     <nav>
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar bg-base-100 shadow-sm px-16">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -25,52 +36,19 @@ const Navbar = () => {
               tabIndex="-1"
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {links}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <img className="w-8" src={Logo} alt="Hero IO Logo" />
+          <a className="text-xl uppercase ml-1">hero.io</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {links}
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Button</a>
+          <Link to="https://github.com/theophildio" target="_blank" className="btn bg-linear-to-r from-[#632EE3] to-[#9F62F2] text-white"><img src={Git} alt="GitHub icon" />Contribute</Link>
         </div>
       </div>
     </nav>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Banner from "../../components/Banner/Banner";
 
 const Home = () => {
     return (
         <div>
-            <h1>This is Home page</h1>
+            <Banner />
         </div>
     );
 };
